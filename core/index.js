7eca59c5eb2981ecdb2d8d514d884597f020d1a3
Klemen Kenda <klemen.kenda@ijs.si>
2018-11-01 04:58:04 +0100

    Adding core module.

---
+++ core/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    StreamModel: require('./stream_model.js')
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+let core = require('./core/index.js');
 let drift_detection = require('./drift_detection/index.js');
 
 module.exports = {
+    Core: core,
     DriftDetection: drift_detection
 }
