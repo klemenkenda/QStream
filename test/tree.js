b71f4315325e035c4f12db53a6e388792a2945a9
Klemen Kenda <klemen.kenda@ijs.si>
2018-11-02 22:20:12 +0100

    Adding Hoeffding tree properties documentation.

--- test/tree.js
+++ test/tree.js
@@ -3,8 +3,10 @@ let QStream = require('../index.js');
 
 describe('tree', function() {
     describe('Hoeffding tree', function() {
-        it('instantiating stream model throws error', function() {
-            let ht = new QStream.Tree.HoeffdingTree(1,2);
+        it('instantiating Hoeffding tree returns object of type HoeffdingTree', function() {
+            let ht = new QStream.Tree.HoeffdingTree();
+            assert.equal(typeof(ht), 'object');
+            assert.equal(ht.constructor.name, 'HoeffdingTree');
         });
     })
 })
--- tree/hoeffding_tree.js
+++ tree/hoeffding_tree.js
@@ -5,19 +5,19 @@ class HoeffdingTree extends StreamModel {
     /**
      * Hoeffding Tree or VFDT.
      *
-     * @param {*} max_byte_size
-     * @param {*} memory_estimate_period
-     * @param {*} grace_period
-     * @param {*} split_criterion
-     * @param {*} split_confidence
-     * @param {*} tie_threshold
-     * @param {*} binary_split
-     * @param {*} stop_mem_management
-     * @param {*} remove_poor_atts
-     * @param {*} no_preprune
-     * @param {*} leaf_prediction
-     * @param {*} nb_threshold
-     * @param {*} nominal_attributes
+     * @param {integer} max_byte_size           Maximum memory consumed by the tree.
+     * @param {integer} memory_estimate_period  Number of instances between memory consumption checks.
+     * @param {integer} grace_period            Number of instances a leaf should observe between split attempts.
+     * @param {string} split_criterion          Split criterion to use (possible strings 'gini' - Gini Index, 'info_gain' - information gain).
+     * @param {float} split_confidence          Allowed error in split decision, a value closer to 0 takes longer to decide.
+     * @param {float} tie_threshold             Threshold below which a split will be forced to break ties.
+     * @param {boolean} binary_split            If true, only allow binary splits.
+     * @param {boolean} stop_mem_management     If true, stop growing as soon as memory limit is hit.
+     * @param {boolean} remove_poor_atts        If true, disable poor attributes.
+     * @param {boolean} no_preprune             If true, disable pre-pruning.
+     * @param {string} leaf_prediction          Prediction mechanism used at leafs ('mc' - majority class, 'nb' - Naive Bayes, 'nba' - Naive Bayer Adaptive).
+     * @param {integer} nb_threshold            Number of instances a leaf should observe before allowing Naive Bayes.
+     * @param {array} nominal_attributes        List of Nominal attributes. If emtpy, then assume that all attributes are numerical.
      */
     constructor(max_byte_size = Number.MAX_SAFE_INTEGER,
         memory_estimate_period = 1000000,
@@ -49,7 +49,8 @@ class HoeffdingTree extends StreamModel {
         this.nominal_attributes = nominal_attributes;
     }
 
-
 }
+
+
 
 module.exports = HoeffdingTree;
