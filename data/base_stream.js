5e9495684a633ac6338ca105522f057721ffb4f0
Matej <mc77.cu@gmail.com>
2018-11-12 12:12:32 +0100

    corrections for waveform generator and test

--- data/base_stream.js
+++ data/base_stream.js
@@ -1,52 +1,90 @@
 class Stream{
 
-    constructor(){
+    constructor() {
         this.n_samples = 0;
         this.n_targets = 0;
-        this.n_features = 0
-        this.n_num_features = 0
-        this.n_cat_features = 0
-        this.n_classes = 0
+        this.n_features = 0;
+        this.n_num_features = 0;
+        this.n_cat_features = 0;
+        this.n_classes = 0;
         this.cat_features_idx = []
-        this.current_sample_x = null
-        this.current_sample_y = null
-        this.sample_idx = 0
-        this.feature_names = null
-        this.target_names = null
-        this.target_values = null
-        this.name = null
+        this.current_sample_x = null;
+        this.current_sample_y = null;
+        this.sample_idx = 0;
+        this.feature_names = null;
+        this.target_names = null;
+        this.target_values = null;
+        this.name = null;
     }
     
-    prepare_for_use(){
+    prepare_for_use() {
+        /*
+         * Prepare the stream for use. Can be the reading of a file, or
+         * the generation of a function, or anything necessary for the
+         * stream to work after its initialization.
+         *
+         * Notes
+         * -----
+         * Every time a stream is created this function has to be called.
+         */
         throw 'Not implemented';
     }
 
-    last_sample(){
+    last_sample() {
         /** 
-        * Retrieves last `batch_size` samples in the stream.
+         * Retrieves last `batch_size` samples in the stream.
          */
         return ([this.current_sample_x, this.current_sample_y]);
     }
 
-    is_restartable(){
+    is_restartable() {
+        /*
+         * Determine if the stream is restartable.
+         * Returns
+         * -------
+         * Boolean
+         *    tTrue if stream is restartable.
+         */
         return true;
     }
 
-    restart(){
-        this.prepare_for_use()
+    restart() {
+        /*
+         * Restart the stream.
+         */
+        this.prepare_for_use();
     }
 
-    n_remaining_samples(){
+    n_remaining_samples() {
+        /*
+         * Returns the estimated number of remaining samples.
+         *
+         * Returns
+         * -------
+         * int
+         *   Remaining number of samples. -1 if infinite (e.g. generator)
+         */
         return (-1);
     }
 
-    has_more_samples(){
+    has_more_samples() {
+        /* Checks if stream has more samples.
+         *
+         * Returns
+         * -------
+         * Boolean
+         *    true if stream has more samples.
+         */
         return (true);
     }
 
-    get_data_info(){
-        let info = this.name + '\n' +this.n_targets + ' targets\n'+ this.n_classes + ' classes\n' + this.n_features + ' features';
+    get_data_info() {
+        let info = this.name + '\n' + this.n_targets + ' targets\n' + this.n_classes + ' classes\n' + this.n_features + ' features';
         return (info);
+    }
+
+    get_class_type() {
+        return 'stream';
     }
 }
 module.exports = Stream;
--- data/waveform_generator.js
+++ data/waveform_generator.js
@@ -1,8 +1,20 @@
 let Stream = require('./base_stream.js')
 let Utils = require('./utils.js')
 
-class WaveformGenerator extends Stream{
-    constructor(has_noise = false, random_state = null){
+class WaveformGenerator extends Stream {
+    /* WaveformGenerator
+     *
+     * Generates instances with 21 numeric attributes and 3 classes, based
+     * on a random differentiation of some base waveforms. Supports noise 
+     * addition, but in this case the generator will have 40 attribute 
+     * instances
+     * 
+     * Parameters
+     * ----------
+     * has_noise: bool
+     *    if True additional 19 insignificant will be added. (Default: False) */
+
+    constructor(has_noise = false, random_state = null) {
         super();
         this.original_random_state = random_state; // random state not used
         this.random_state = null; 
@@ -18,127 +30,127 @@ class WaveformGenerator extends Stream{
         this.configure();
     }
 
-    configure(){
-        if (this.has_noise){
+    configure() {
+        if (this.has_noise) {
             this.n_num_features = 40; // number of features when has_noise = true
         }
         this.n_features = this.n_num_features;
         this.feature_names =[];
-        for(let i = 0; i<this.n_features; i++){
+        for(let i = 0; i<this.n_features; i++) {
             this.feature_names.push('att_num_' + i);
         }
         this.target_names = ['target_0']; 
         this.target_values = []
-        for(let i = 0; i<this.n_classes; i++){
+        for(let i = 0; i<this.n_classes; i++) {
             this.target_values.push(i);
         }
     }
 
-    has_noise(){
+    has_noise() {
         /** Retrieve the value of the option: add noise.
-        *
-        * Returns
-        * -------
-        * Boolean
-        *   True if the noise is added.
-        */
+         *
+         * Returns
+         * -------
+         * Boolean
+         *   True if the noise is added.
+         */
         
         return (this.has_noise)
     }
 
-    set_has_noise(has_noise){
+    set_has_noise(has_noise) {
         /** Set the value of the option: add noise.
-        *
-        * Parameters
-        * ----------
-        * has_noise: Boolean
-        *
-        */
-        if (typeof(has_noise) === 'boolean'){
+         *
+         * Parameters
+         * ----------
+         * has_noise: Boolean
+         *
+         */
+        if (typeof(has_noise) === 'boolean') {
             this.has_noise = has_noise;
             this.configure();
         }
         else{throw TypeError('has_noise should be boolean, \'' + typeof(has_noise) +  '\' was passed')} //error
     }
 
-    prepare_for_use(){
+    prepare_for_use() {
         /**
-        * Should be called before generating the samples.
-        */
+         * Should be called before generating the samples.
+         */
         
         //self.random_state = check_random_state(self._original_random_state)
         this.sample_idx = 0
     }
 
-    next_sample(batch_size = 1){
+    next_sample(batch_size = 1) {
         /**  next_sample
-        *
-        * An instance is generated based on the parameters passed. If noise 
-        * is included the total number of features will be 40, if it's not
-        * included there will be 21 attributes. In both cases there is one 
-        * classification task, which chooses one between three labels.
-        * 
-        * After the number of attributes is chosen, the algorithm will randomly
-        * choose one of the hard coded waveforms, as well as random multipliers. 
-        * For each attribute, the actual value generated will be a a combination 
-        * of the hard coded functions, with the multipliers and a random value.
-        * 
-        * Furthermore, if noise is added the features from 21 to 40 will be
-        * replaced with a random normal value.
-        * 
-        * Parameters
-        * ----------
-        * batch_size: int
-        *     The number of samples to return.
-        *     
-        * Returns
-        * -------
-        * tuple or tuple list
-        *     Return a tuple with the features matrix and the labels matrix 
-        *     for the batch_size samples that were requested.
-        */
+         *
+         * An instance is generated based on the parameters passed. If noise 
+         * is included the total number of features will be 40, if it's not
+         * included there will be 21 attributes. In both cases there is one 
+         * classification task, which chooses one between three labels.
+         * 
+         * After the number of attributes is chosen, the algorithm will randomly
+         * choose one of the hard coded waveforms, as well as random multipliers. 
+         * For each attribute, the actual value generated will be a a combination 
+         * of the hard coded functions, with the multipliers and a random value.
+         * 
+         * Furthermore, if noise is added the features from 21 to 40 will be
+         * replaced with a random normal value.
+         * 
+         * Parameters
+         * ----------
+         * batch_size: int
+         *     The number of samples to return.
+         *     
+         * Returns
+         * -------
+         * tuple or tuple list
+         *     Return a tuple with the features matrix and the labels matrix 
+         *     for the batch_size samples that were requested.
+         */
 
-        let rand = new Utils()
+        let utils = new Utils()
         let data = [];
         let dimensions = [batch_size, this.n_features + 1]
         for (let i = 0; i < dimensions[0]; ++i) {
             data.push(new Array(dimensions[1]).fill(0));
         };
 
-        for(let j = 0; j < batch_size; j++){
+        for(let j = 0; j < batch_size; j++) {
             this.sample_idx += 1;
             let group = Math.floor(Math.random() * this.n_classes);
             let choice_a = group == 2 ? 1 : 0;
             let choice_b = group == 0 ? 1 : 2;
             let multiplier_a = Math.random();
             let multiplier_b = 1.0 - multiplier_a;
-            for (let i = 0; i <= this.n_featu-1; i++){
-                data[j][i] = multiplier_a * this.h_function[choice_a][i] + multiplier_b * this.h_function[choice_b][i] + rand.rand_gauss();
+            for (let i = 0; i <= this.n_featu - 1; i++) {
+                data[j][i] = multiplier_a * this.h_function[choice_a][i] + multiplier_b * this.h_function[choice_b][i] + utils.rand_gauss();
             };
 
-            if(this.has_noise){
-                for (let i = this.n_featu; i < this.n_num_features; i++){
-                    data[j][i] = rand.rand_gauss();
+            if(this.has_noise) {
+                for (let i = this.n_featu; i < this.n_num_features; i++) {
+                    data[j][i] = utils.rand_gauss();
                 };
             };
 
-            data[j][data[j].length -1] = group;     
+            data[j][data[j].length - 1] = group;     
         }
         this.current_sample_x = [];
         this.current_sample_y = [];
-        for (let k = 0; k < batch_size; k++){
+        for (let k = 0; k < batch_size; k++) {
             this.current_sample_x.push(data[k].slice(0, this.n_num_features));
             this.current_sample_y.push(data[k][this.n_num_features]);
         }
         return ([this.current_sample_x, this.current_sample_y]);
     }
 
-    get_info(){
+    get_info() {
         let info = 'Waveform Generator:'
-                    +'\n n_classes: ' + this.n_classes  
-                    +'\n n_num_features: ' + this.n_num_features
-                    +'\n n_cat_features: ' + this.n_cat_features
-                    +'\n has_noise: ' + this.has_noise
+                    + '\n n_classes: ' + this.n_classes  
+                    + '\n n_num_features: ' + this.n_num_features
+                    + '\n n_cat_features: ' + this.n_cat_features
+                    + '\n has_noise: ' + this.has_noise
                     //+'\n random_state: ' + this._original_random_state;
 
         return (info);
